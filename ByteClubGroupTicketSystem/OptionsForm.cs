bf65db4df415d9ef1fdd315c6e4707cc577d8b08
KarloParagas <karloparagas@gmail.com>
2020-01-28 14:02:38 -0800

    Options dialog box is also now functional.

--- ByteClubGroupTicketSystem/OptionsForm.cs
+++ ByteClubGroupTicketSystem/OptionsForm.cs
@@ -24,23 +24,70 @@ namespace ByteClubGroupTicketSystem
 
         private void OkBtn_Click(object sender, EventArgs e)
         {
-            //Create an Options object and set the user's input to it's properties
-            Options input = new Options()
+            if (IsDataValid() == true) 
             {
-                MinutesPerWindow = Convert.ToInt32(MinutesPerWindowTxt.Text),
-                GuestsPerWindow = Convert.ToInt32(GuestsPerWindowTxt.Text),
-                StartTime = Convert.ToDateTime(StartTimeTxt.Text),
-                EndTime = Convert.ToDateTime(EndTimeTxt.Text),
-                FirstTicketNumber = Convert.ToInt32(FirstTicketNumTxt.Text)
-            };
+                //Create an Options object and set the user's input to it's properties
+                Options input = new Options()
+                {
+                    MinutesPerWindow = Convert.ToInt32(MinutesPerWindowTxt.Text),
+                    GuestsPerWindow = Convert.ToInt32(GuestsPerWindowTxt.Text),
+                    StartTime = Convert.ToDateTime(StartTimeTxt.Text),
+                    EndTime = Convert.ToDateTime(EndTimeTxt.Text),
+                    FirstTicketNumber = Convert.ToInt32(FirstTicketNumTxt.Text)
+                };           
+                //Assign the Options object with all the extrated user input to the UserInput instance
+                //so it can be accessed in other forms
+                UserInput = input;
 
-            //Assign the Options object with all the extrated user input to the UserInput instance
-            //so it can be accessed in other forms
-            UserInput = input;
-
-            DialogResult = DialogResult.OK;
+                DialogResult = DialogResult.OK;
+            }
         }
 
-        //TODO: Add Validation
+        private bool IsDataValid()
+        {
+            if (IsPresent() == true && IsInputValid() == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsInputValid()
+        {
+            //Variables to be used for testing, for valid integer and date time input
+            int validInteger;
+            DateTime validDateTime;
+
+            if (!DateTime.TryParse(StartTimeTxt.Text, out validDateTime) ||
+                !DateTime.TryParse(EndTimeTxt.Text, out validDateTime))
+            {
+                MessageBox.Show("Start time and End time must be a valid date time input. Ex: (10:00 AM/PM)");
+                return false;
+            }
+            if (!int.TryParse(MinutesPerWindowTxt.Text, out validInteger) ||
+                !int.TryParse(GuestsPerWindowTxt.Text, out validInteger) ||
+                !int.TryParse(FirstTicketNumTxt.Text, out validInteger))
+            {
+                MessageBox.Show("Minutes per window, Guests per window, and " +
+                                "First ticket number must be a valid integer input.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPresent()
+        {
+            if (string.IsNullOrWhiteSpace(MinutesPerWindowTxt.Text) ||
+                string.IsNullOrWhiteSpace(GuestsPerWindowTxt.Text) ||
+                string.IsNullOrWhiteSpace(StartTimeTxt.Text) ||
+                string.IsNullOrWhiteSpace(EndTimeTxt.Text) ||
+                string.IsNullOrWhiteSpace(FirstTicketNumTxt.Text))
+            {
+                MessageBox.Show("Cannot submit an empty form");
+                return false;
+            }
+            return true;
+        }
     }
 }
