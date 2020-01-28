bd4557f37cd398dcbd3ae3c3c1096ae2ffce9421
KarloParagas <karloparagas@gmail.com>
2020-01-28 07:19:58 -0800

    Created Options class

---
+++ ByteClubGroupTicketSystem/Options.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteClubGroupTicketSystem
+{
+    /// <summary>
+    /// A class that represents the time slot for assigning tickets
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Represents how many minutes it is per window
+        /// </summary>
+        public int MinutesPerWindow { get; set; }
+
+        /// <summary>
+        /// Represents how guests are allowed per window
+        /// </summary>
+        public int GuestsPerWindow { get; set; }
+
+        /// <summary>
+        /// Represents the start time 
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Represents the end time
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        /// <summary>
+        /// Represents the first ticket number to be issued
+        /// </summary>
+        public int FirstTicketNumber { get; set; }
+    }
+}
