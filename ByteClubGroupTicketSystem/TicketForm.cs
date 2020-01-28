75a91dc87da23d3a204a58eca20f41c94c10ae54
KarloParagas <karloparagas@gmail.com>
2020-01-28 06:11:59 -0800

    Created TicketForm and OptionsForm design

--- ByteClubGroupTicketSystem/TicketForm.cs
+++ ByteClubGroupTicketSystem/TicketForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ByteClubGroupTicketSystem
 {
-    public partial class Form1 : Form
+    public partial class TicketForm : Form
     {
-        public Form1()
+        public TicketForm()
         {
             InitializeComponent();
         }
