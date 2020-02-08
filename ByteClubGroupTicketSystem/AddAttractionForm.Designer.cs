f78e583f1be5ff3c874b2f029cf18343618cac22
KarloParagas <karloparagas@gmail.com>
2020-02-07 23:23:18 -0800

    Implemented being able to add an attraction to the form and database

---
+++ ByteClubGroupTicketSystem/AddAttractionForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ByteClubGroupTicketSystem
+{
+    partial class AddAttractionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AttractionNameTxt = new System.Windows.Forms.TextBox();
+            this.AddAttractionNameBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Attraction Name:";
+            // 
+            // AttractionNameTxt
+            // 
+            this.AttractionNameTxt.Location = new System.Drawing.Point(104, 23);
+            this.AttractionNameTxt.Name = "AttractionNameTxt";
+            this.AttractionNameTxt.Size = new System.Drawing.Size(161, 20);
+            this.AttractionNameTxt.TabIndex = 1;
+            // 
+            // AddAttractionNameBtn
+            // 
+            this.AddAttractionNameBtn.Location = new System.Drawing.Point(126, 60);
+            this.AddAttractionNameBtn.Name = "AddAttractionNameBtn";
+            this.AddAttractionNameBtn.Size = new System.Drawing.Size(121, 23);
+            this.AddAttractionNameBtn.TabIndex = 2;
+            this.AddAttractionNameBtn.Text = "Add Attraction";
+            this.AddAttractionNameBtn.UseVisualStyleBackColor = true;
+            this.AddAttractionNameBtn.Click += new System.EventHandler(this.AddAttractionNameBtn_Click);
+            // 
+            // AddAttractionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 95);
+            this.Controls.Add(this.AddAttractionNameBtn);
+            this.Controls.Add(this.AttractionNameTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "AddAttractionForm";
+            this.Text = "Add Attraction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AttractionNameTxt;
+        private System.Windows.Forms.Button AddAttractionNameBtn;
+    }
+}
---
+++ ByteClubGroupTicketSystem/AddAttractionForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ByteClubGroupTicketSystem
+{
+    public partial class AddAttractionForm : Form
+    {
+        public AddAttractionForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adds a single attraction to the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddAttractionNameBtn_Click(object sender, EventArgs e)
+        {
+            //Create an attraction object
+            Attraction a = new Attraction() 
+            {
+                AttractionName = AttractionNameTxt.Text
+            };
+
+            try
+            {
+                AttractionDb.Add(a);
+                MessageBox.Show("Attraction Successfully added");
+                DialogResult = DialogResult.OK;
+            }
+            catch 
+            {
+                MessageBox.Show("We're currently having server issues");
+            }
+        }
+
+        //TODO: Add input validation
+    }
+}
