ec808d20648803bbb12a525f2d9b73594e744676
KarloParagas <karloparagas@gmail.com>
2020-02-08 13:30:15 -0800

    Added TimeSlot class

---
+++ ByteClubGroupTicketSystem/TimeSlot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteClubGroupTicketSystem
+{
+    public class TimeSlot
+    {
+        [Key]
+        public int TimeSlotId { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        public TimeSpan TimeSlotLength { get; set; }
+
+        public override string ToString() 
+        {
+            return $"{StartTime.ToShortTimeString()} - {EndTime.ToShortTimeString()}";
+        }
+    }
+}
