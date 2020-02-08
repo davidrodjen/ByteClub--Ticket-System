cb6557024d77100e577973d0a1641e70f807386d
KarloParagas <karloparagas@gmail.com>
2020-02-07 20:53:30 -0800

    Added an Attraction class

---
+++ ByteClubGroupTicketSystem/Attraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteClubGroupTicketSystem
+{
+    /// <summary>
+    /// A class that represents a single attraction
+    /// </summary>
+    public class Attraction
+    {
+        [Key]
+        public int AttractionId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string AttractionName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{AttractionName}";
+        }
+    }
+}
