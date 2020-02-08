baa9ed194e6bd194c6f4034d64ed523013fd4bf3
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-07 18:12:17 -0800

    Deleted old main form and created a new form to appropriate to project issues.

---
+++ ByteClubGroupTicketSystem/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ByteClubGroupTicketSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TicketPurchase_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please enter ticket purchase information in the next window.");
+            OptionsForm form = new OptionsForm();
+            form.ShowDialog();
+        }
+    }
+}
