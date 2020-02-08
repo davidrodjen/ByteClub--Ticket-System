44fbc47508dc77117c705e5be90c8666bf3076f4
KarloParagas <karloparagas@gmail.com>
2020-02-07 23:36:22 -0800

    Added an Options class that can store an attraction, a time slot, and the number of guests for that attraction and time

--- ByteClubGroupTicketSystem/Options.cs
+++ ByteClubGroupTicketSystem/Options.cs
@@ -7,33 +7,30 @@ using System.Threading.Tasks;
 namespace ByteClubGroupTicketSystem
 {
     /// <summary>
-    /// A class that represents the time slot for assigning tickets
+    /// Represents the fast pass options
     /// </summary>
     public class Options
     {
         /// <summary>
-        /// Represents how many minutes it is per window
+        /// No argument constructor. It initializes everything to default value without having explicity
         /// </summary>
-        public int MinutesPerWindow { get; set; }
+        public Options() 
+        {
+        }
 
         /// <summary>
-        /// Represents how guests are allowed per window
+        /// The name of the attraction
         /// </summary>
-        public int GuestsPerWindow { get; set; }
+        public string Attraction { get; set; }
 
         /// <summary>
-        /// Represents the start time 
+        /// Represents the time slot for entry
         /// </summary>
-        public DateTime StartTime { get; set; }
+        public DateTime TimeSlot { get; set; }
 
         /// <summary>
-        /// Represents the end time
+        /// Represents the number of guests for that entry
         /// </summary>
-        public DateTime EndTime { get; set; }
-
-        /// <summary>
-        /// Represents the first ticket number to be issued
-        /// </summary>
-        public int FirstTicketNumber { get; set; }
+        public int NumberOfGuests { get; set; }
     }
 }
